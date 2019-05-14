dfff9e560a577058d9e6e96ad70b70af57aa1754
pankaj <pankaj@Abhis-MacBook-Air.local>
2019-05-14 08:23:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 1
-        </a>
-      </header>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCount = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  render() {
+    return <div data-test='component-app'>
+      <h1 data-test='counter-view'>Counter Value Is: {this.state.counter}</h1>
+      <button data-test='counter-button' onClick={this.incrementCount}>Increment Value</button>
     </div>
-  );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
 });
+
+/*
+* This is factory function to create a ShallowWrapper for the App Component
+@function setup
+@param {object} props - Component props for Setup
+@param {object} state - Initial state for Setup
+@return {ShallowWrapper}
+*/
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state)
+  return wrapper
+}
+
+/**
+ * 
+ * @param {ShallowWrapper} wrapper - Shallow wrapper to search within 
+ * @param {string} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper} 
+ */
+const findByAttribute = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
+describe('Test for increment counter', () => {
+  test('renders without crashing', () => {
+    const wrapper = setup();
+    const renderComp = findByAttribute(wrapper, 'component-app');
+    expect(renderComp.length).toBe(1);
+  });
+
+  test('test render of button', () => {
+    const wrapper = setup();
+    const renderComp = findByAttribute(wrapper, 'counter-button');
+    expect(renderComp.length).toBe(1);
+  });
+
+  test('test render of counter display', () => {
+    const wrapper = setup();
+    const renderComp = findByAttribute(wrapper, 'counter-view');
+    expect(renderComp.length).toBe(1);
+  })
+
+  test('test intial value should be 0 of counter', () => {
+    const wrapper = setup();
+    let initialState = wrapper.state('counter');
+    expect(initialState).toBe(0);
+  })
+
+  test('test check increment of counter', () => {
+    const counter = 7;
+    const wrapper = setup(null, { counter });
+    const button = findByAttribute(wrapper, 'counter-button');
+    button.simulate('click');
+    const updateCounter=findByAttribute(wrapper, 'counter-view');
+    expect(updateCounter.text()).toContain(counter+1);
+  })
+
+})
+
